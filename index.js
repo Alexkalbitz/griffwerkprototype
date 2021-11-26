fd5e5474aec861ee93f5d7ce2f84e31982318cd6
Alex Kalbitz <alexkalbitz@yahoo.de>
2021-11-26 12:39:40 +0100

    file

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-let test = "hello "
 
-console.log(test)
+const express = require('express')
+const app = express()
+const port = 3000
 
+var fs = require('fs');
+fs.readFile(__dirname + '/link.json', function (err, data) {
+    if (err) {
+        throw err;
+    }
+    var newArr = JSON.parse(data)
+    console.log(data.toString());
+});
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get("/test/:id", (req, res) => {
+    console.log(req.params.id)
+
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
